954d9386b8d03837813c7782a1b31dc8c84e379f
PatrickLR7 <patrickrx7@gmail.com>
2018-05-28 23:53:33 -0600

    Commit inicial.

---
+++ app/src/main/java/com/example/patricklr7/tcu501_2images1word/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.patricklr7.tcu501_2images1word;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
