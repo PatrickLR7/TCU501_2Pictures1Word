31254beabd9fccee0263207da4c1ee931ffb41c7
Patrick <patrickrx7@gmail.com>
2018-06-05 04:35:46 -0600

    Clase Common agregada

---
+++ app/src/main/java/com/example/patricklr7/tcu501_2images1word/Common/Common.java
@@ -0,0 +1,15 @@
+package com.example.patricklr7.tcu501_2images1word.Common;
+
+/**
+ * Created by Patrick on 04/06/2018.
+ */
+
+public class Common {
+    public static char userSubmitAnswer;
+    public static int count = 1;
+    public static String[] alphabetChars = {
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+
+
+}
--- app/src/main/java/com/example/patricklr7/tcu501_2images1word/InOutDoorActivity.java
+++ app/src/main/java/com/example/patricklr7/tcu501_2images1word/InOutDoorActivity.java
@@ -2,14 +2,31 @@ package com.example.patricklr7.tcu501_2images1word;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
 
+import com.example.patricklr7.tcu501_2images1word.Adapter.GridViewAnswerAdapter;
+import com.example.patricklr7.tcu501_2images1word.Adapter.GridViewLettersAdapter;
 import com.example.patricklr7.tcu501_2images1word.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Patrick on 2/6/2018.
  */
 
 public class InOutDoorActivity extends AppCompatActivity {
+
+    public List<String> suggestSource = new ArrayList<>();
+    public GridViewAnswerAdapter answerAdapter;
+    public GridViewLettersAdapter lettersAdapter;
+    public Button btnSubmit;
+    public GridView gridViewAnswer, gridViewLetters;
+    public ImageView imgViewQuestion;
+    public int [] image_list = {};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
