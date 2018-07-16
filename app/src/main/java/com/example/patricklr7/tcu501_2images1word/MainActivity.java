8b2481856cbbcfc4581c112567e3262a99fc5ebe
PatrickLR7 <patrickrx7@gmail.com>
2018-07-16 17:53:50 -0600

    Instrucciones agregadas.

--- app/src/main/java/com/example/patricklr7/tcu501_2images1word/MainActivity.java
+++ app/src/main/java/com/example/patricklr7/tcu501_2images1word/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.patricklr7.tcu501_2images1word;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.AdapterView;
-import android.widget.GridView;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.patricklr7.tcu501_2images1word.Common.Common;
 
@@ -57,6 +64,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    /**
+     * Metodo para crear el menu.
+     * @param menu layout con el menu.
+     * @return true si se crea correctamente.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+
+    /**
+     * Metodo para realizar una accion al seleccionar items del menu.
+     * @param item una opcion del menu.
+     * @return true si realiza la accion correctamente.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.menu_Ins:
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialogTheme);
+                builder.setTitle(getText(R.string.tIns));
+                builder.setMessage(getText(R.string.instrucciones));
+                builder.setIcon(R.drawable.tcu501logo);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+
+                }
+            });
+                AlertDialog dialog = builder.show();
+                // Must call show() prior to fetching text view
+                TextView messageView = (TextView)dialog.findViewById(android.R.id.message);
+                messageView.setGravity(Gravity.CENTER);
+        }
+        return true;
     }
 
 
